66d8977e2d34950eb09176de033e2fba7ffab978
Laurynas Ruškys <bilbas@gmail.com>
2023-04-30 16:21:02 +0300

    Output as tsv

--- index.mjs
+++ index.mjs
@@ -13,6 +13,26 @@ const container = database.container(containerName);
 async function main() {
     const { resources } = await container.items.query('SELECT * FROM c')
         .fetchAll();
+
+    let latestResultDate = "2022-11-29T00:00:00";
+    let latestResultRows = null;
+
+    const csvHeaders = ['DATE', ...Array(24).keys()].map(i => String(i).padStart(2, '0'));
+    console.log(csvHeaders.join('\t'));
+
+    resources.forEach(element => {
+        if (element.response.data.LatestResultDate != latestResultDate && latestResultRows != null) {
+            const csvRow = [latestResultDate, ...latestResultRows.filter(r => !r.IsExtraRow).map(row => row.Columns[0].Value)];
+            console.log(csvRow.join('\t'));
+        }
+
+        latestResultRows = element.response.data.Rows;
+        latestResultDate = element.response.data.LatestResultDate;
+        // console.log(element);
+    });
+
+    const csvRow = [latestResultDate, ...latestResultRows.filter(r => !r.IsExtraRow).map(row => row.Columns[0].Value)];
+    console.log(csvRow.join('\t'));
 }
 
 main().catch((error) => {
