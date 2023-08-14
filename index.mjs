51a086568414f6406a0c2318a48084553ea08c23
Laurynas Ruškys <bilbas@gmail.com>
2023-08-14 23:42:08 +0300

    Print output in Europe/Vilnius timezone to be string-compatible with ESO reports

--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 import { DefaultAzureCredential } from "@azure/identity";
 import { CosmosClient } from "@azure/cosmos";
 
+process.env.TZ = 'Europe/Oslo';
+
 const endpoint = "https://nordpool-ahead-hourly.documents.azure.com:443/";
 const databaseName = 'nordpool';
 const containerName = 'hourly';
@@ -9,6 +11,33 @@ const options = { endpoint, aadCredentials: credential };
 const client = new CosmosClient(options);
 const database = client.database(databaseName);
 const container = database.container(containerName);
+const vilniusFormat = new Intl.DateTimeFormat('sv-SE',
+    {
+        timeZone: 'Europe/Vilnius',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+        timeZoneName: 'longOffset'
+    })
+
+function getVilniusISOString(date) {
+    const parts = vilniusFormat.formatToParts(date);
+    var tzOffset = parts[12].value.slice(3); // Remove GMT from GMT+03:00
+    return `${parts[0].value}-${parts[2].value}-${parts[4].value}T${parts[6].value}:${parts[8].value}:${parts[10].value}${tzOffset}`;
+}
+
+function printTsvResults(latestResultRows) {
+    latestResultRows.filter(r => !r.IsExtraRow).forEach(row => {
+        const startTime = new Date(Date.parse(row.StartTime));
+        const endTime = new Date(Date.parse(row.EndTime));
+        const csvRow = [getVilniusISOString(startTime), getVilniusISOString(endTime), row.Columns[0].Value];
+        console.log(csvRow.join('\t'));
+    });
+}
 
 async function main() {
     const { resources } = await container.items.query('SELECT * FROM c')
@@ -17,15 +46,12 @@ async function main() {
     let latestResultDate = "2022-11-29T00:00:00";
     let latestResultRows = null;
 
-    const csvHeaders = ['StartTime CE(S)T', 'EndTime CE(S)T', 'EUR/MWh'];
+    const csvHeaders = ['StartTime', 'EndTime', 'EUR/MWh'];
     console.log(csvHeaders.join('\t'));
 
     resources.forEach(element => {
         if (element.response.data.LatestResultDate != latestResultDate && latestResultRows != null) {
-            latestResultRows.filter(r => !r.IsExtraRow).forEach(row => {
-                const csvRow = [row.StartTime, row.EndTime, row.Columns[0].Value];
-                console.log(csvRow.join('\t'));
-            });
+            printTsvResults(latestResultRows);
         }
 
         latestResultRows = element.response.data.Rows;
@@ -33,10 +59,7 @@ async function main() {
         // console.log(element);
     });
 
-    latestResultRows.filter(r => !r.IsExtraRow).forEach(row => {
-        const csvRow = [row.StartTime, row.EndTime, row.Columns[0].Value];
-        console.log(csvRow.join('\t'));
-    });
+    printTsvResults(latestResultRows);
 }
 
 main().catch((error) => {
